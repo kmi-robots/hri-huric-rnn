94ebad2a43c998a313c82d7f74c194ee812a7089
Martino Mensio <martinomensio@outlook.it>
2018-05-25 18:25:43 +0100

    evaluate things from prediction stored on files WIP

--- joint/metrics.py
+++ joint/metrics.py
@@ -1,9 +1,12 @@
+import os
 import numpy as np
 import numpy.ma as ma
 from sklearn.metrics import f1_score, precision_recall_fscore_support
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+from . import data
 
 def get_data_from_sequence_batch(true_batch, pred_batch, eos_token):
     """Extract data from a batch of sequences：
@@ -77,3 +80,48 @@ def plot_history(file_name, history):
     plt.grid()
     print(file_name)
     plt.savefig(file_name)
+
+def clean_predictions(decoder_prediction_batch, intent_prediction_batch, true_batch):
+    """Given the raw outputs of the network, provides back a usable representation, with the same structure as the training samples"""
+    # from time-major matrix to sample-major
+    decoder_prediction_batch = np.transpose(decoder_prediction_batch, [1, 0])
+
+    samples = [{
+        'words': gold['words'][:gold['length']],
+        'intent_pred': intent_prediction,
+        'intent_true': gold['intent'],
+        'length': gold['length'],
+        'slots_pred': [slot if (slot != '<EOS>' and slot != '<PAD>') else 'O' for slot in decoder_prediction[:gold['length']]],
+        'slots_true': gold['slots'][:gold['length']],
+        'file': gold['file'],
+        'start_token_id': gold['start_token_id'],
+        'end_token_id': gold['end_token_id'],
+        'id': gold['id']
+    } for (gold, decoder_prediction, intent_prediction) in zip(true_batch, decoder_prediction_batch, intent_prediction_batch)]
+
+    return samples
+
+def evaluate_epoch(epoch_data):
+
+    intent_pred, intent_true, slots_pred, slots_true = zip(*[(s['intent_pred'], s['intent_true'], s['slots_pred'], s['slots_true']) for s in epoch_data])
+
+    intent_measures = precision_recall_f1_for_intents(intent_true, intent_pred)
+
+    # argument-based
+    slots_arguments_true = data.sequence_iob_to_ents(slots_true)
+    slots_arguments_pred = data.sequence_iob_to_ents(slots_pred)
+    slots_measures = precision_recall_f1_slots(slots_arguments_true, slots_arguments_pred)
+    slots_cond_measures = precision_recall_f1_slots_conditioned_intent(slots_arguments_true, slots_arguments_pred, intent_true, intent_pred)
+
+    # token-based
+    all_slots_true = [x for y in slots_true for x in y]
+    all_slots_pred = [x for y in slots_pred for x in y]
+    # TODO change the name of the function
+    token_slots_measures = precision_recall_f1_for_intents(all_slots_true, all_slots_pred)
+
+    return {
+        'intent': intent_measures,
+        'slots': slots_measures,
+        'slots_cond': slots_cond_measures,
+        'token_based_slots': token_slots_measures
+    }
