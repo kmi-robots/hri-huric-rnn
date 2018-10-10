48744c2a38097aea36a2427a1da2e9f3cf85d282
Martino Mensio <martinomensio@outlook.it>
2018-10-10 17:51:22 +0100

    also do cross validation

--- evaluate_dataset_combination.sh
+++ evaluate_dataset_combination.sh
@@ -18,6 +18,8 @@ export LABEL_EMB_SIZE=64
 export LSTM_SIZE=128
 #export MAX_EPOCHS=100
 
+# cross fold HuRIC
+MODE=cross DATASET=huric_eb/modern_right make train_joint
 # 80%H --> 20%H
 DATASET=huric_eb/modern_right make train_joint
 # 80%H + 100%FN_subset --> 20%H
@@ -57,14 +59,25 @@ MODE=test DATASET=huric_eb/modern_right MODEL_PATH=${FATE_MODEL} make train_join
 
 # collapse histories (find max among epochs)
 export COMMON_FOLDER_NAME=_loss_both_slottype_full_we_large_recurrent_cell_lstm_attention_both_three_stages_true_highway___hyper\:LABEL_EMB_SIZE\=64\,LSTM_SIZE\=128\,BATCH_SIZE\=2\,MAX_EPOCHS\=100
-python nlunetwork/results_aggregator.py nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME} modern_right
-mv nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated.json nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/huric_eb/modern_right/
 
+python nlunetwork/results_aggregator.py nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME} modern_right
+mv nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated.json nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated_modern_righ.json
+
+# also the cross validation
+python nlunetwork/results_aggregator.py nlunetwork/results/dataset_combination/cross${COMMON_FOLDER_NAME} modern_right
+mv nlunetwork/results/dataset_combination/cross${COMMON_FOLDER_NAME}/aggregated.json nlunetwork/results/dataset_combination/cross${COMMON_FOLDER_NAME}/aggregated_modern_righ.json
+
+# huric with framenet, test 20%
 python nlunetwork/results_aggregator.py nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME} with_framenet
-mv nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated.json nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/huric_eb/with_framenet/
+mv nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated.json nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated_with_framenet.json
 
 # this one will capture fate and framenet
 python nlunetwork/results_aggregator.py nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME} modern_both
-mv nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated_modern_both.json
+mv nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated.json nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated_modern_both.json
 
-# TODO with_fate and with_fate_and_framenet
+# with_fate and with_framenet_and_fate
+python nlunetwork/results_aggregator.py nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME} with_fate
+mv nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated.json nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated_with_fate.json
+
+python nlunetwork/results_aggregator.py nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME} with_framenet_and_fate
+mv nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated.json nlunetwork/results/dataset_combination/eval${COMMON_FOLDER_NAME}/aggregated_with_framenet_and_fate.json
