ba1c90f7081045279d5256aeed0e40810fefc2df
Martino Mensio <martinomensio@outlook.it>
2018-10-24 16:27:27 +0100

    fixing evaluation? maybe

--- evaluate.sh
+++ evaluate.sh
@@ -6,18 +6,57 @@
 
 set -e
 
+best_config_h=`python select_best.py nlunetwork/results/tuning/aggregated_huric.json`
+best_config_fn=`python select_best.py nlunetwork/results/tuning/aggregated_fn.json`
+
+
+# 1. cross validate on HuRIC with the tuned parameters from HuRIC
+set -a
+source nlunetwork/results/tuning/${best_config_h}/${DATASET}/config.env
+set +a
+export OUTPUT_FOLDER=tuning/optimized/
+export MODE=cross
+python nlunetwork/main.py
+
+python nlunetwork/results_aggregator.py nlunetwork/results/tuning/optimized/ ${DATASET}
+
+
+# 2. train FrameNet and test on HuRIC (transfer learning?)
+# 2.1. with the hyperparams from FrameNet
+# 2.2. with the hyperparams from HuRIC
+
+# train on full FrameNet with the hyperparam from FrameNet
+set -a
+source nlunetwork/results/tuning/${best_config_fn}/${DATASET}/config.env
+set +a
+export OUTPUT_FOLDER=tuning/optimized/
+export MODE=train_all
+python nlunetwork/main.py
+
+# train all FrameNet subset on the hyperparams of HuRIC
+set -a
+source nlunetwork/results/tuning/${best_config_h}/${DATASET}/config.env
+set +a
+export DATASET=framenet/subset_both
+export OUTPUT_FOLDER=tuning/optimized/
+export MODE=train_all
+python nlunetwork/main.py
+
+# determine the paths of the models
+best_config_fn_train_all=${best_config_fn/cross/train_all}
+best_config_h_train_all=${best_config_h/cross_nested/train_all}
+# TODO asggiustare sta riga che il dataset non quadra
+
+# now test all with fn hyperparam
+export MODE=test_all
+export MODEL_PATH=nlunetwork/results/tuning/optimized/${best_config_fn_train_all}/framenet/subset_both/
 export DATASET=huric_eb/modern_right
-# chosen architecture: three stages with attention
-export THREE_STAGES=true_highway
-export ATTENTION=both
+python nlunetwork/main.py
 
+# now test all with h hyperparams
+export MODE=test_all
+export MODEL_PATH=nlunetwork/results/tuning/optimized/${best_config_h_train_all}/framenet/subset_both/
+export DATASET=huric_eb/modern_right
+python nlunetwork/main.py
 
-# this time test on folds 1-4 and test on 5
-export MODE=eval
-
-export BATCH_SIZE=2
-
-# the chosen hyperparams
-export LABEL_EMB_SIZE=64
-export LSTM_SIZE=128
-make train_joint
+python nlunetwork/results_aggregator.py nlunetwork/results/tuning/optimized/ ${DATASET}
--- hyperparam_tuning_nested.sh
+++ hyperparam_tuning_nested.sh
@@ -23,16 +23,7 @@ done
 python nlunetwork/results_aggregator.py nlunetwork/results/tuning/ ${DATASET}
 
 mv nlunetwork/results/tuning/aggregated.json nlunetwork/results/tuning/aggregated_huric.json
-best_config=`python select_best.py nlunetwork/results/tuning/aggregated_huric.json`
 
-set -a
-source nlunetwork/results/tuning/${best_config}/${DATASET}/config.env
-set +a
-export OUTPUT_FOLDER=tuning/optimized/
-export MODE=cross
-python nlunetwork/main.py
-
-python nlunetwork/results_aggregator.py nlunetwork/results/tuning/optimized/ ${DATASET}
 
 # for FrameNet
 export DATASET=framenet/subset_both
@@ -54,15 +45,7 @@ done
 python nlunetwork/results_aggregator.py nlunetwork/results/tuning/ ${DATASET}
 
 mv nlunetwork/results/tuning/aggregated.json nlunetwork/results/tuning/aggregated_fn.json
-best_config=`python select_best.py nlunetwork/results/tuning/aggregated_fn.json`
 
-set -a
-source nlunetwork/results/tuning/${best_config}/${DATASET}/config.env
-set +a
-export OUTPUT_FOLDER=tuning/optimized/
-export MODE=test_all
-export MODEL_PATH=nlunetwork/results/tuning/${best_config}/${DATASET}
-export DATASET=huric_eb/modern
-python nlunetwork/main.py
-
-python nlunetwork/results_aggregator.py nlunetwork/results/tuning/optimized/ ${DATASET}
+echo "Hyperparameter tuning results:"
+python select_best.py nlunetwork/results/tuning/aggregated_huric.json
+python select_best.py nlunetwork/results/tuning/aggregated_fn.json
