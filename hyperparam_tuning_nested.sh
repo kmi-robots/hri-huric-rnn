1211d6f31a07f4ca4d7ac64a436a9177c3f1f22c
Martino Mensio <martinomensio@outlook.it>
2018-10-29 15:08:20 +0000

    tuning on the real framenet lexical unit files

--- hyperparam_tuning_nested.sh
+++ hyperparam_tuning_nested.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export MAX_EPOCHS=1
+export MAX_EPOCHS=20
 export OUTPUT_FOLDER=tuning
 
 CONF_FILES=configurations/conf_*.env
@@ -22,7 +22,7 @@ python nlunetwork/results_aggregator.py nlunetwork/results/tuning/ ${DATASET}
 mv nlunetwork/results/tuning/aggregated.json nlunetwork/results/tuning/aggregated_huric.json
 
 
-# for FrameNet
+# for FrameNet fulltexts
 export DATASET=framenet/subset_both
 export MODE=cross
 
@@ -33,9 +33,24 @@ done
 
 # aggregate the epochs
 python nlunetwork/results_aggregator.py nlunetwork/results/tuning/ ${DATASET}
-mv nlunetwork/results/tuning/aggregated.json nlunetwork/results/tuning/aggregated_fn.json
+mv nlunetwork/results/tuning/aggregated.json nlunetwork/results/tuning/aggregated_fn_ft.json
+
+# for FrameNet lu
+export MAX_EPOCHS=10 # more samples, less epochs
+export DATASET=framenet/modern_lu_subset_right
+export MODE=cross
+
+for f in $CONF_FILES
+do
+    CONFIG_FILE=${f} python nlunetwork/main.py
+done
+
+# aggregate the epochs
+python nlunetwork/results_aggregator.py nlunetwork/results/tuning/ ${DATASET}
+mv nlunetwork/results/tuning/aggregated.json nlunetwork/results/tuning/aggregated_fn_lu.json
 
 
 echo "Hyperparameter tuning results:"
 python select_best.py nlunetwork/results/tuning/aggregated_huric.json
-python select_best.py nlunetwork/results/tuning/aggregated_fn.json
+python select_best.py nlunetwork/results/tuning/aggregated_fn_ft.json
+python select_best.py nlunetwork/results/tuning/aggregated_fn_lu.json
